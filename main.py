8db5b9c38c88fdfe990f45841226d8540b150b8f
Александр Пирожков <sanchellopirozhkov@mail.ru>
2021-11-08 12:06:07 +0300

    error correction

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from ya import Yandex
 from vk import VK_request
 import json
 
-TOKEN_VK = "31d9be8fad58c49998fd1909dde2d9729eb9899136b79db75c2389f7f60610bde03a6aa8578d2f66e4a6e"
-ID = "170907239"
-TOKEN_YA = "AQAAAAAFHjZVAADLW7FYr7-e10sAvKQ_yQdr_n4"
+TOKEN_VK = ""
+ID = ""
+TOKEN_YA = ""
 
 if __name__ == '__main__':
     my_vk = VK_request(TOKEN_VK, ID)
